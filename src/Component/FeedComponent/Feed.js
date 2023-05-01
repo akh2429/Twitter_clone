dcbb0fdbeb618197accbd75a9ad3d1af573ca8ec
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2023-05-01 10:24:02 +0530

    like functionality added

--- src/Component/FeedComponent/Feed.js
+++ src/Component/FeedComponent/Feed.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import f from "./Feed.module.css"
 import { AiOutlineHeart } from 'react-icons/ai'
 import { RxLoop } from 'react-icons/rx'
 import { FaRegComment } from 'react-icons/fa'
 
-
 function Feed({ data }) {
+    const [likeCount, setLikeCount] = useState(data.likeCount);
+    const [liked, setLiked] = useState(false);
+
+    const handleLikeClick = () => {
+        if (liked) {
+            setLikeCount(likeCount - 1);
+        } else {
+            setLikeCount(likeCount + 1);
+        }
+        setLiked(!liked);
+    };
 
     return (
         <div className={f.parent}>
@@ -17,15 +28,18 @@ function Feed({ data }) {
                     <p className={f.content}>{data.content}</p>
                     <div className={f.Body}>
                         <img src={data.image} className={f.bodyImage} />
-                        <div className={f.Footer} >
-                            <div><AiOutlineHeart /> {data.likeCount}</div>
+                        <div className={f.Footer}>
+                            <div onClick={handleLikeClick}>
+                                <AiOutlineHeart style={{ color: liked ? 'red' : 'grey' }} /> {likeCount}
+                            </div>
                             <div><FaRegComment /> {data.commentCount}</div>
                             <div><RxLoop /> {data.reTweetsCount}</div>
                         </div>
                     </div>
-                </div >
+                </div>
             </div>
-        </div>)
+        </div>
+    );
 }
 
 export default Feed;
