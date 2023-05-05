e5fccf0b383862348ed0d75c7968b3bbd89f2cb8
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2023-05-06 01:56:53 +0530

    responsive complete

--- src/Component/Feed Top/FeedTop.js
+++ src/Component/Feed Top/FeedTop.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import Post from './Post'
 import f from "./Feed.module.css"
+import { BsTwitter } from 'react-icons/bs'
 
 export default function FeedTop() {
+  const screenwidth = window.innerWidth
   return (
     <div className={f.main}>
       <div className={f.box}>
         <div className={f.first}>
-          <h2>Home</h2>
+          { screenwidth > 430 ?<h2>Home</h2> : 
+          <div className={f.res}>
+            <div className={f.left}> <img src="https://tse4.mm.bing.net/th?id=OIP.Ii15573m21uyos5SZQTdrAHaHa&pid=Api&P=0" alt="img" /> </div>
+            <div className={f.right}><BsTwitter /></div>
+            
+          </div>
+          
+          }
+          
         </div>
         <div className={f.second}>
           <div>For You</div>
--- src/Component/Navbar/Nav.js
+++ src/Component/Navbar/Nav.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import n from './Nav.module.css'
+import { BiHomeCircle } from 'react-icons/bi'
+import { BsSearch } from 'react-icons/bs'
+import { GrNotification } from 'react-icons/gr'
+import { HiOutlineMail } from 'react-icons/hi'
 
 
 export default function Nav() {
   return (
     <nav className={n.main}>
-      <div><BiHomeCircle/></div>
-      <div><BsSearch/></div>
-      <div><GrNotification/></div>
-      <div><HiOutlineMail/></div>
+      <div className={n.icon}><BiHomeCircle/></div>
+      <div className={n.icon}><BsSearch/></div>
+      <div className={n.icon}><GrNotification/></div>
+      <div className={n.icon}><HiOutlineMail/></div>
     </nav>
   )
 }
